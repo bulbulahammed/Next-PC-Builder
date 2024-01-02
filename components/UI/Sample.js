94de76a822c4d9ec63a7949228e40b8199f1fda4
Bulbul Ahammed <mdbulbulahammed@gmail.com>
2024-01-02 23:32:08 +0600

    Brand New Beast

--- components/UI/Sample.js
+++ components/UI/Sample.js
@@ -5,25 +5,25 @@ import { FaRegCheckCircle } from "react-icons/fa";
 const Sample = () => {
     return (
         <div className="bg-black text-white">
-            <div className="flex place-content-center items-center mx-10">
+            <div className="flex place-content-center items-center mx-10 py-20">
                 <div className="w-1/3 text-left">
                     <div className=" text-3xl md:text-4xl lg:text-5xl font-bold">
                         <h3>Brand New Beast</h3>
                     </div>
                     <div className="py-20">
-                        <div className="flex place-items-center">
+                        <div className="flex place-items-center my-1">
                             <h2 className="text-green-500 mr-3"><FaRegCheckCircle /></h2>
                             <p>Premium Custom PC</p>
                         </div>
-                        <div className="flex place-items-center">
+                        <div className="flex place-items-center  my-1">
                         <h2 className="text-green-500 mr-3"><FaRegCheckCircle /></h2>
                             <p>Ready For High Performance.</p>
                         </div>
-                        <div className="flex place-items-center">
+                        <div className="flex place-items-center  my-2">
                         <h2 className="text-green-500 mr-3"><FaRegCheckCircle /></h2>
                             <p>Premium Full Steel Chassis.</p>
                         </div>
-                        <div className="flex place-items-center">
+                        <div className="flex place-items-center  my-2">
                         <h2 className="text-green-500 mr-3"><FaRegCheckCircle /></h2>
                             <p>Streamer Friendly.</p>
                         </div>
@@ -44,7 +44,24 @@ const Sample = () => {
                     height={300}
                     />
                 </div>
-                <div className="w-1/3 text-center">3</div>
+                <div className="w-1/3 text-center">
+                    <div>
+                      <h2>Processor</h2>
+                      <p>Processor</p>
+                    </div>
+                    <div>
+                      <h2>Graphics Card</h2>
+                      <p>Processor</p>
+                    </div>
+                    <div>
+                      <h2>Memory</h2>
+                      <p>Processor</p>
+                    </div>
+                    <div>
+                      <h2>Cooling</h2>
+                      <p>Processor</p>
+                    </div>
+                </div>
             </div>
         </div>
     );
